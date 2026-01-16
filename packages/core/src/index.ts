3cff8e731b388ed0e05365c590bb6d689100b3d6
shikr <cristobal.rod0907@gmail.com>
2026-01-16 13:19:11 -0600

    feat(core): add resolveUnit function
    Basic resolver function, this is a base resolver, this can be useful for more complex resolvers.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
+import { resolve } from './resolver.js'
 import type { Unit, UnitPayload } from './types.js'
 import { createUnitInternal } from './unit.js'
 
@@ -5,6 +6,10 @@ export function createUnit<T, const N extends string, D extends readonly Unit[]>
   payload: UnitPayload<T, N, D>
 ): Unit<T, N, D> {
   return createUnitInternal(Symbol(payload.name), payload)
+}
+
+export function resolveUnit<T>(unit: Unit<T>): Promise<T> {
+  return resolve(unit)
 }
 
 export type * from './types.js'

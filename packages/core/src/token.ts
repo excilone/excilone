e2a02c02d5739484d9a6e325735a1cd78279074b
shikr <cristobal.rod0907@gmail.com>
2026-01-17 22:39:44 -0600

    feat(core): add `declareToken` function

---
+++ packages/core/src/token.ts
@@ -0,0 +1,43 @@
+import { __bind } from './constants.js'
+import { MissingBindingError } from './errors.js'
+import type { Binding, Token } from './types.js'
+import { createUnitInternal } from './unit.js'
+
+function createBindingInternal<T, const N extends string>(
+  id: symbol,
+  name: N,
+  value: T
+): Binding<T, N> {
+  return {
+    ...createUnitInternal<T, N, []>(id, {
+      name,
+      using: [],
+      factory: () => value,
+    }),
+    [__bind]: true,
+    as<NewName extends string>(newName: NewName): Binding<T, NewName> {
+      return createBindingInternal(id, newName, value)
+    },
+  }
+}
+
+export function createTokenInternal<T, const N extends string>(
+  id: symbol,
+  name: N
+): Token<T, N> {
+  const providerFn = (data: T): Binding<T, N> => createBindingInternal(id, name, data)
+
+  return Object.assign(providerFn, {
+    ...createUnitInternal<T, N, []>(id, {
+      name,
+      using: [],
+      factory: () => {
+        throw new MissingBindingError(name)
+      },
+    }),
+    [__bind]: false as const,
+    as<NewName extends string>(newName: NewName): Token<T, NewName> {
+      return createTokenInternal(id, newName)
+    },
+  })
+}

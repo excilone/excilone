9894651477d9a2e6e24c5924413407d3dcd78d24
shikr <cristobal.rod0907@gmail.com>
2026-01-18 23:12:38 -0600

    test(core): add tests using tokens

---
+++ packages/core/tests/token.test.ts
@@ -0,0 +1,49 @@
+import { describe, expect, it } from 'vitest'
+import { createUnit, declareToken, resolveUnit } from '../src/index.js'
+
+describe('Token and Binding creation', () => {
+  it('should create a token and binding correctly', async () => {
+    const createToken = declareToken<string>()
+
+    const NameToken = createToken('name')
+
+    const GreetingUnit = createUnit({
+      name: 'greeting',
+      using: [NameToken('World')],
+      factory: (deps) => `Hello, ${deps.name}`,
+    })
+
+    await expect(resolveUnit(GreetingUnit)).resolves.toBe('Hello, World')
+  })
+
+  it('should use the nearest binding', async () => {
+    const createToken = declareToken<number>()
+
+    const AgeToken = createToken('age')
+
+    const PersonUnit = createUnit({
+      name: 'person',
+      using: [AgeToken],
+      factory: (deps) => ({ age: deps.age }),
+    })
+
+    const MainUnit = createUnit({
+      name: 'main',
+      using: [
+        AgeToken(30),
+        PersonUnit,
+        AgeToken(12).as('childAge'),
+        PersonUnit.as('childPerson'),
+      ],
+      factory: (deps) => ({
+        personAge: deps.person.age,
+        childPersonAge: deps.childPerson.age,
+      }),
+    })
+
+    await expect(resolveUnit(MainUnit)).resolves.toEqual({
+      personAge: 30,
+      childPersonAge: 12,
+    })
+  })
+})

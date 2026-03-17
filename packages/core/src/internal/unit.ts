23ea947897e13401d31cbb96e6c7e5c54027e9ee
shikr <cristobal.rod0907@gmail.com>
2026-03-16 20:30:28 -0600

    feat(core): use bind instead of type in `Meta`
    The only use of the `type` property is to identify if a unit is a binding, so with a boolean
    property is enough.

--- packages/core/src/internal/unit.ts
+++ packages/core/src/internal/unit.ts
@@ -1,22 +1,22 @@
 import type { CoreUnit, Unit } from '../types.js'
-import { __meta, type UnitKey, type UnitType } from './meta.js'
+import { __meta, type UnitKey } from './meta.js'
 
 export function createUnit<
   T,
   S extends boolean,
   K extends UnitKey | null,
   D extends readonly Unit[],
->(id: symbol, sync: S, key: K, type: UnitType, payload: CoreUnit<T, S, D>) {
+>(id: symbol, sync: S, key: K, bind: boolean, payload: CoreUnit<T, S, D>) {
   return {
     ...payload,
     [__meta]: {
       identity: id,
-      type,
+      bind,
       key,
       sync,
     },
     as(newKey) {
-      return createUnit(id, sync, newKey, type, payload)
+      return createUnit(id, sync, newKey, bind, payload)
     },
   } as Unit<S extends true ? T : Awaited<T>, S, K, D>
 }
